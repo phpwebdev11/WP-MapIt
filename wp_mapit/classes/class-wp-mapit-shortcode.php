b65ec5eae78c7e76e8beb4428a92eb62020ee7f1
Niriksha Patel <developer.semicolon@gmail.com>
2026-05-22 15:19:14 +0530

    WP Mapit - Add Filter Functionality in map - For shortcode
    WP Mapit - Add Filter Functionality in map - For shortcode

--- wp_mapit/classes/class-wp-mapit-shortcode.php
+++ wp_mapit/classes/class-wp-mapit-shortcode.php
@@ -71,13 +71,50 @@ if ( ! class_exists( 'Wp_Mapit_Shortcode' ) ) {
 		 */
 		public static function wp_mapit_map( $atts ) {
 
-			$map_id = isset( $atts['id'] ) ? intval( $atts['id'] ) : 0;
+			$map_id = (int) ( $atts['id'] ?? 0 );
+			$tags   = $atts['tags'] ?? '';
+			$tags   = self::wp_mapit_get_tag_ids_from_slugs( $tags );
 
+			// If map ID is provided, generate the map for that ID.
 			if ( $map_id > 0 ) {
 				if ( Wp_Mapit_Multipin_Map::has_map( $map_id ) ) {
-					return Wp_Mapit_Multipin_Map::generate_map( $map_id );
+					return Wp_Mapit_Multipin_Map::generate_map( $map_id, $tags );
 				}
 			}
+		}
+
+		/**
+		 * Function to convert tag slugs to tag IDs.
+		 *
+		 * @since 3.2.0
+		 * @static
+		 * @access public
+		 * @param string $slugs Comma-separated string of tag slugs.
+		 * @return array Returns an array of tag IDs corresponding to the provided slugs.
+		 */
+		 public static function wp_mapit_get_tag_ids_from_slugs( $slugs ) {
+			// Trim and split the slugs into an array.
+			$slugs = array_filter( array_map( 'trim', explode( ',', $slugs ) ) );
+
+			// If no slugs provided, return empty array.
+			if ( empty( $slugs ) ) {
+				return [];
+			}
+
+			// Get terms by slugs.
+			$terms = get_terms([
+				'taxonomy'   => 'wp_mapit_tag',
+				'slug'       => $slugs,
+				'hide_empty' => false,
+			]);
+
+			// If error or no terms found, return empty array.
+			if ( is_wp_error( $terms ) ) {
+				return [];
+			}
+
+			// Return array of term IDs.
+			return wp_list_pluck( $terms, 'term_id' );
 		}
 	}
 
